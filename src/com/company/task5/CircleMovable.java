4dbc59ad6bb0d5fce83f93226096482827afb623
Alex <Alexzz123@yandex.ru>
2020-09-18 10:01:26 +0300

    done task 5
    Added CircleMovable

---
+++ src/com/company/task5/CircleMovable.java
@@ -0,0 +1,13 @@
+package com.company.task5;
+
+public class CircleMovable extends Circle implements Movable{
+
+    public CircleMovable(Point center, double radius) {
+        super(center, radius);
+    }
+
+    @Override
+    public void move(int x, int y) {
+        ((PointMovable)getCenter()).move(1,1);
+    }
+}
