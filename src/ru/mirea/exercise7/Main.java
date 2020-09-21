c1005b58f7dbb469a780b38872697b30cbae5954
artemmakarevich1997 <artemmakarevich1997@gmail.com>
2020-09-21 17:05:51 +0300

    add explanation of 7th exercise

---
+++ src/ru/mirea/exercise7/Main.java
@@ -0,0 +1,13 @@
+package ru.mirea.exercise7;
+
+public class Main {
+    public static void main(String[] args){
+        Dish plate = new Plate(1,10);
+
+        Dish pan = new Pan(20,100);
+
+        plate.calcVolume();
+        ((Pan)pan).makeBorsh();
+
+    }
+}
