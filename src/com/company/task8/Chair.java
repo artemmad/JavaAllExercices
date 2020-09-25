600108e28874b6eb8183efb765d4e2878d5a1088
Alex <Alexzz123@yandex.ru>
2020-09-25 10:50:31 +0300

    Task 8
    Added abstract class Furniture, chair and sofa

---
+++ src/com/company/task8/Chair.java
@@ -0,0 +1,17 @@
+package com.company.task8;
+
+public class Chair extends Furniture {
+    public Chair(int cost, String name) {
+        super(cost, name);
+    }
+
+    @Override
+    public int getCost() {
+        return super.getCost();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
---
+++ src/com/company/task8/Sofa.java
@@ -0,0 +1,17 @@
+package com.company.task8;
+
+public class Sofa extends Furniture {
+    public Sofa(int cost, String name) {
+        super(cost, name);
+    }
+
+    @Override
+    public int getCost() {
+        return super.getCost();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
