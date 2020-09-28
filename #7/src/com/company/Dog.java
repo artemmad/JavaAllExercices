98eb8ed98b00247c836aa2499258c70540644c25
AniViRus <56561161+AniViRus@users.noreply.github.com>
2020-09-28 19:05:11 +0300

    Revert "Практические работы №7,8 Бойко В.Н. ИНБО-10-19"
    This reverts commit 78812a7b7f17a3b13f0e2abee6f323546bc56abc.

--- #7/src/com/company/Dog.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public abstract class Dog {
-    public String toStringAbstract() {
-        return "Dog{}";
-    }
-}
--- #8/src/com/company/Furniture.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public abstract class Furniture {
-    String material;
-
-    public Furniture(String material) {
-        this.material = material;
-    }
-}
