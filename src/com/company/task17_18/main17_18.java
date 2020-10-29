e647f2e60b617c023b5f835f1f2a751b7add3383
Alex <Alexzz123@yandex.ru>
2020-10-29 19:00:33 +0300

    Done17_18
    Added classes first,second and third

--- src/com/company/task17_18/main17_18.java
+++ src/com/company/task17_18/main17_18.java
@@ -4,7 +4,9 @@ public class main17_18 {
     public static void main(String[] args) {
 //        First first = new First();
 //        first.check();
-        Second second=new Second();
-        second.check();
+//        Second second=new Second();
+//        second.check();
+        Third third = new Third();
+        third.check();
     }
 }
